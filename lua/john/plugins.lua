491a3199f642d61b7a68d6b71c89e068766f9acb
John Kaustinen <jokausti@gmail.com>
2023-11-19 17:00:24 +0200

    Added formatting with null-ls

--- lua/john/plugins.lua
+++ lua/john/plugins.lua
@@ -101,6 +101,27 @@ local plugins = {
             -- insert_at_start = true,
             -- …etc.
         },
+    },
+    {
+        'nvimtools/none-ls.nvim',
+        -- opts = {
+        --     on_attach = function(client, bufnr)
+        --         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        --         if client.supports_method("textDocument/formatting") then
+        --             vim.api.nvim_clear_autocmds({
+        --                 group = augroup,
+        --                 buffer = bufnr,
+        --             })
+        --             vim.api.nvim_create_autocmd("BufWritePre", {
+        --                 group = augroup,
+        --                 buffer = bufnr,
+        --                 callback = function()
+        --                     vim.lsp.buf.format({ async = false })
+        --                 end,
+        --             })
+        --         end
+        --     end
+        -- }
     }
 }
 return plugins
